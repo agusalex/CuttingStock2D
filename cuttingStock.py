2d676e3018cd88d281a59db294f8b6eeeead9c1c
agusalex <agustin.c.alexnader@gmail.com>
2020-11-29 00:22:12 -0300

    minor changes

--- cuttingStock.py
+++ cuttingStock.py
@@ -2,67 +2,20 @@ import os
 import time
 from shutil import copyfile
 from rectangle import Rectangle
-import grapher as Grapher
 import subprocess
+import numpy as np
+from lib import *
+
 
 #MURO
-ancho_muro = 45
-alto_muro = 45
+ancho_muro = 47
+alto_muro = 47
 #POSTERS
-posters = [(5,5),(10,10)]
+posters = [(7,5),(10,12)]
 #Minimos
-ancho_min_poster = 5
-alto_min_poster = 5
-
-
-
-
-def valuesToZPLList(pos):
-    tostr = ""
-    for value in pos:
-        tostr = tostr + str(value) + "\n"
-    return tostr[0:len(tostr)-1]
-
-def valuesToZPLTuple(pos):
-    tostr = ""
-    for value in pos:
-        tostr = tostr + str(value[0]) + ";" +str(value[1])+ "\n"
-    return tostr[0:len(tostr)-1]
-
-def writeFile(filename, content):
-    f = open(filename, "w")
-    f.write(content)
-    f.close()
-
-
-def calculateSteps(step, maxSize):
-    w_steps = []
-    w = 0
-    while(w+step < maxSize):
-        w_steps.append(w)
-        w = w + step
-    return w_steps
-
-
-def parseRectangles(filename):
-    rectangles = []
-    with open(filename) as fp:
-        line = fp.readline()
-        line = fp.readline()
-        line = fp.readline()
-        cnt = 1
-        end = False
-        print("\n###############################################")
-        print("Solution:")
-        while line and not end:
-            line = fp.readline()
-            if(len(line)>1):
-                recstr = line.split('#')      
-                rectangles.append(
-                    Rectangle(int(recstr[1]), int(recstr[2]), int(recstr[3]), int(recstr[4].split(' ')[0])))
-                print(recstr[1] + ", "+recstr[2] + ", " + recstr[3] + ", " + recstr[4].split(' ')[0])
-    return rectangles
-
+minimos = findMinimun(posters)
+ancho_min_poster = minimos[0]
+alto_min_poster = minimos[1]
 
 print("\n###############################################")
 print("Posibilidades:")
@@ -72,10 +25,12 @@ print(x_pos)
 print(y_pos)
 print("Posters:")
 print(posters)
+print("Minimos:")
+print(minimos)
 for x in x_pos:
-    Grapher.addVline(x)
+    addVline(x)
 for y in y_pos:
-    Grapher.addHline(y)
+    addHline(y)
 
 writeFile("posters.txt", valuesToZPLTuple(posters))
 writeFile("altos.txt", valuesToZPLList(y_pos))
@@ -86,7 +41,7 @@ filename = 'solution.sol'
 correct = subprocess.run(
     ['scip','-c','read cuttingStock2d.zpl','-c','set display verblevel 3' ,'-c', 'optimize',  '-c', 'write solution {}'.format(filename),'-c','quit'])
 rectangles = parseRectangles(filename)
-Grapher.addRectangles(rectangles)
-Grapher.draw("out.png")
+addRectangles(rectangles)
+draw("out.png")
 print("###############################################")
 print("Graph written in out.png")
---
+++ lib.py
@@ -0,0 +1,92 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from rectangle import Rectangle
+import random
+
+def findMinimun(posters):
+    ancho = []
+    alto = []
+    for tup in posters:
+        ancho.append(tup[0])
+        alto.append(tup[1])
+    return (min(ancho),min(alto))
+
+def valuesToZPLList(pos):
+    tostr = ""
+    for value in pos:
+        tostr = tostr + str(value) + "\n"
+    return tostr[0:len(tostr)-1]
+
+def valuesToZPLTuple(pos):
+    tostr = ""
+    for value in pos:
+        tostr = tostr + str(value[0]) + ";" +str(value[1])+ "\n"
+    return tostr[0:len(tostr)-1]
+
+def writeFile(filename, content):
+    f = open(filename, "w")
+    f.write(content)
+    f.close()
+
+
+def calculateSteps(step, maxSize):
+    w_steps = []
+    w = 0
+    while(w+step < maxSize):
+        w_steps.append(w)
+        w = w + step
+    return w_steps
+
+
+def parseRectangles(filename):
+    rectangles = []
+    with open(filename) as fp:
+        line = fp.readline()
+        line = fp.readline()
+        line = fp.readline()
+        cnt = 1
+        end = False
+        print("\n###############################################")
+        print("Solution:")
+        while line and not end:
+            line = fp.readline()
+            if(len(line)>1):
+                recstr = line.split('#')      
+                rectangles.append(
+                    Rectangle(int(recstr[1]), int(recstr[2]), int(recstr[3]), int(recstr[4].split(' ')[0])))
+                print(recstr[1] + ", "+recstr[2] + ", " + recstr[3] + ", " + recstr[4].split(' ')[0])
+    return rectangles
+
+
+def create_rectangle(rectangle: Rectangle):
+    # get the right map, and get the color from the map
+    color = matplotlib.cm.jet(rectangle.width + rectangle.height + random.randint(0,100))
+    rec = plt.Rectangle((rectangle.x, rectangle.y), rectangle.width,
+                        rectangle.height, color=color, zorder=1, alpha=0.25)
+    add_shape(rec)
+
+
+def add_shape(patch):
+    ax = plt.gca()
+    ax.add_patch(patch)
+    plt.axis('scaled')
+
+
+def draw(filename):
+    plt.savefig(filename)
+
+def addRectangles(drawlist):
+  #  matplotlib.use( 'tkagg' )
+    for item in drawlist:
+        create_rectangle(item)
+def addHline(y):
+    plt.axhline(y=y,alpha=0.1,color='tab:orange')
+
+def addVline(x):
+    plt.axvline(x=x,alpha=0.1,color='tab:orange')
+
+    
+
+
+# plt.imsave('demo')
