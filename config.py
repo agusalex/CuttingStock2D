30128d5fd8f080b3d40fbb88dc76924852d68ffb
agusalex <agustin.c.alexnader@gmail.com>
2020-12-11 17:16:40 -0300

    minor change

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ ancho_muro = parsePostersInput(os.getenv("ANCHO","240"))[0]
 alto_muro = parsePostersInput(os.getenv("ALTO","520"))[0]
 
 # POSTERS agregar vertical y horizontal. Sintaxis =  ancho;alto#cantidad, ancho;alto#cantidad 
-raw_posters_cant = parsePostersInput(os.getenv("POSTERS","36;254#4,254;36#4,104;55#8,55;104#8,40;30#9,30;40#9"))
+raw_posters_cant = parsePostersInput(os.getenv("POSTERS","36;254#4, 254;36#4, 104;55#8, 55;104#8, 40;30#9, 30;40#9"))
 posters_cant = filterNonFittingPosters(raw_posters_cant,ancho_muro,alto_muro)
 posters = extractPosters(posters_cant)
 anchos_posters = getXorYPosters(posters,ancho_muro,'ancho')
