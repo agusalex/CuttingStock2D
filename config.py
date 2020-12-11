91a58b82f01a4f621b68a85d6973189722d2883c
gsturla <gsturla@finnegans.com.ar>
2020-12-10 22:30:45 -0300

    Avances-12/10

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ ancho_muro = 240
 alto_muro = 520
 # POSTERS agregar siempre vertical y horizontal, siempre numero par
 posters = [(254,36),(36,254),(104,55),(55,104),(40,30),(30,40)]
+#Cantidad de posters
+cantPostersA = 4
+cantPostersB = 8
+cantPostersC = 9
 #cantidades = [20,20]
 #cantidadesXPoster = getCantPerCutHandV(cantidades,posters) #(cantidad,ancho*alto)
 #Posibles anchos y altos de posters que entran en el muro
