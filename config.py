5328c446363e2436fd0c1e4adaebda49f3449c7a
agusalex <agustin.c.alexnader@gmail.com>
2020-12-15 00:10:08 -0300

    threads now  working

--- config.py
+++ config.py
@@ -29,11 +29,12 @@ def parseListPlainTextInput(input):
 
 anchos_muro = parseListPlainTextInput(os.getenv("ANCHO","240,240"))
 altos_muro = parseListPlainTextInput(os.getenv("ALTO","520,286"))
-verbose = False
+verbose = True
 threaded = True
 max_threads = 2
 costos_muro = parseListPlainTextInput(os.getenv("COSTO","100,70"))
-raw_posters_with_amount = parseRectanglesPlainTextInput(os.getenv("POSTERS","36;254#20, 254;36#20"))#, 104;55#8, 55;104#8, 40;30#9, 30;40#9"))
-
 # POSTERS agregar vertical y horizontal. Sintaxis =  ancho;alto#cantidad, ancho;alto#cantidad 
+raw_posters_with_amount = parseRectanglesPlainTextInput(os.getenv("POSTERS","36;254#4, 254;36#4, 104;55#8, 55;104#8, 40;30#9, 30;40#9"))
+
+
 
